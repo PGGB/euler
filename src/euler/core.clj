b4496e1123c01d1c70071c01870f91db9a8e2aa8
PGGB <grizzlyschaf@gmail.com>
2012-06-24 15:19:13 +0200

    Solved Problem 3

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,18 +1,51 @@
 (ns euler.core)
 
+(defn divisible? [n d]
+  (zero? (rem n d)))
+
 (defn fib [a b]
   (lazy-seq (cons a (fib b (+ a b)))))
 
+(defn lazy-primes []
+  (letfn [(append-under-key [h key val]
+         (assoc h key (conj (h key) val)))
+      (run-iterators [h n]
+         (dissoc (reduce #(append-under-key %1 (+ n %2) %2) h (h n)) n))
+      (next-prime [h n]
+         (if (h n) ;; If h has any items, it is not prime
+           (recur (run-iterators h n) (+ n 2))
+           (cons n (lazy-seq
+            (next-prime (append-under-key h (* n n) (+ n n)) (+ n 2))))))]
+    (cons 2 (lazy-seq (next-prime {} 3)))))
+
+(defn prime?
+  "Tests whether a given number is prime."
+  [n]
+  (cond
+    (or (= n 2) (= n 3))          true
+    (or (divisible? n 2) (< n 2)) false
+    :else                         (let [sqrt-n (Math/sqrt n)]
+                                    (loop [i 3]
+                                      (cond
+                                        (divisible? n i) false
+                                        (< sqrt-n i)     true
+                                        :else            (recur (+ i 2)))))))
+
 (defn euler001 []
    (reduce + 
-           (filter #(or (zero? (mod % 3)) (zero? (mod % 5))) (range 1 1000))))
+           (filter #(or (divisible? % 3) (divisible? % 5)) (range 1 1000))))
 
 (euler001)
 
-(fib)
-
-(take-while #(< % 4000000) (fib 1 2))
-
-(defn euler 002 []
+(defn euler002 []
   (reduce + (filter even? (take-while #(< % 4000000) (fib 1 2)))))
 
+(euler002)
+
+(defn euler003 []
+  (loop [x 2]
+    (if (and (divisible? 600851475143 x) (prime? (/ 600851475143 x)))
+      (/ 600851475143 x) 
+      (recur (inc x)))))
+
+(euler003)
